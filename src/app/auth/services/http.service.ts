2742ba722b689d376905dc3007eaaa0025b02803
Henry Miguel Ruiz Reyes <henry.ruizr@ug.edu.ec>
2023-03-03 00:51:08 -0500

    Updated http service

--- src/app/auth/services/http.service.ts
+++ src/app/auth/services/http.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpHandler, HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from 'src/app/auth/interface/user';
+import { Task } from 'src/app/task/interface/task';	
 
 
 @Injectable({
@@ -32,6 +33,18 @@ export class httpService {
         })
       };
       const url = `${this.apiUrl}/Auth/crearUsuario?nombre=${usuario.nombre.toString()}&correo=${usuario.correo.toString()}&contrase%C3%B1a=${usuario.contraseña.toString()}`;
+      console.log(url);
+      return this.http.post(url, "", httpOptions);
+    }
+
+    creaTarea(tarea: Task, id_usuario: string): Observable<any> {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Access-Control-Allow-Origin': '*'
+        })
+      };
+      const url = `${this.apiUrl}/Auth/crearUsuario?id_usuario=${id_usuario}&titulo=${tarea.titulo.toString()}&descripcion=${tarea.descripcion.toString()}&fecha_fin=${tarea.fechaFin.toString()}&fecha_inicio=${tarea.fechaInicio.toString()}&estado=${tarea.estado.toString()}`
       console.log(url);
       return this.http.post(url, "", httpOptions);
     }
